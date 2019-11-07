2c10c115870e38d157b4ebaf439f58892f53e931
FlynnTheInfinite <fabienmorival@gmail.com>
2019-11-07 21:15:43 +0100

    Corrected auras

--- model/Blueprint/BreakTile.js
+++ model/Blueprint/BreakTile.js
@@ -6,7 +6,9 @@ class BreakTile extends Bloc {
 	constructor (src, ctx) {
 
 		super("brktile", src, ctx);
-		this.f = (src, ins) => [ins[0].field.tiles, ins[0].area, ins[0].inFront, ins[0].isEmpty, ins[0].card, ins[0].neighbors, ins[0].adjacents, ins[0].tilesAhead, ins[0].tilesBehind, ins[0].line];
+		this.f = (src, ins) => ins[0].neighbors ?
+			[ins[0].field.tiles, ins[0].area, ins[0].inFront, ins[0].isEmpty, ins[0].card, ins[0].neighbors, ins[0].adjacents, ins[0].tilesAhead, ins[0].tilesBehind, ins[0].line] :
+			[[], ins[0].area, false, false, null, [], [], [], [], []];
 		this.types = [Types.location];
 	}
 }
