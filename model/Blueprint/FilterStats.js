8c5b01fbbeaaafcc38de5bb9751815a80e87ca92
FlynnTheInfinite <fabienmorival@gmail.com>
2019-11-10 23:38:11 +0100

    Bug correction

--- model/Blueprint/FilterStats.js
+++ model/Blueprint/FilterStats.js
@@ -9,10 +9,10 @@ class FilterStats extends Bloc {
 		this.f = (src, ins) => {
 
 			var parsing = value => isNaN(value) ? parseInt(value, 10) : value;
-			var checkmana = target => (ins[0] === null || (target.mana && parsing(target.mana) >= ins[0])) && (ins[1] === null || (target.mana && parsing(target.mana) <= ins[1]));
-			var checkatk = target => (ins[2] === null || (target.atk && parsing(target.atk) >= ins[2])) && (ins[3] === null || (target.atk && parsing(target.atk) <= ins[3]));
-			var checkhp = target => (ins[4] === null || (target.hp && parsing(target.hp) >= ins[4])) && (ins[5] === null || (target.hp && parsing(target.hp) <= ins[5]));
-			var checkrange = target => (ins[6] === null || (target.range && parsing(target.range) >= ins[6])) && (ins[7] === null || (target.range && parsing(target.range) <= ins[7]));
+			var checkmana = target => (ins[0] === null || (!isNaN(target.mana) && parsing(target.mana) >= ins[0])) && (ins[1] === null || (!isNaN(target.mana) && parsing(target.mana) <= ins[1]));
+			var checkatk = target => (ins[2] === null || (!isNaN(target.atk) && parsing(target.atk) >= ins[2])) && (ins[3] === null || (!isNaN(target.atk) && parsing(target.atk) <= ins[3]));
+			var checkhp = target => (ins[4] === null || (!isNaN(target.hp) && parsing(target.hp) >= ins[4])) && (ins[5] === null || (!isNaN(target.hp) && parsing(target.hp) <= ins[5]));
+			var checkrange = target => (ins[6] === null || (!isNaN(target.range) && parsing(target.range) >= ins[6])) && (ins[7] === null || (!isNaN(target.range) && parsing(target.range) <= ins[7]));
 			var cardfilter = target => checkmana(target.eff) && checkatk(target.eff) && checkhp(target.eff) && checkrange(target.eff);
 			
 			return [cardfilter, cardfilter];
