857054edf3c20f269e07ded7edd05cdece14f499
FlynnTheInfinite <fabienmorival@gmail.com>
2019-11-13 06:55:08 +0100

    Compare cards handle missing cards

--- model/Blueprint/CompareCards.js
+++ model/Blueprint/CompareCards.js
@@ -6,7 +6,11 @@ class CompareCards extends Bloc {
 	constructor (src, ctx) {
 
 		super("cmpcards", src, ctx);
-		this.f = (src, ins) => [ins[0].id === ins[1].id, ins[0].cardType === ins[1].cardType, ins[0].area && ins[1].area && ins[0].area.id === ins[1].area.id];
+		this.f = (src, ins) => {
+			if (!ins[0] || !ins[1])
+				return [false, false, false];
+			return [ins[0].id === ins[1].id, ins[0].cardType === ins[1].cardType, ins[0].area && ins[1].area && ins[0].area.id === ins[1].area.id];
+		}
 		this.types = [Types.card, Types.card];
 	}
 }
