bbe5c350e979a014a07223e2322165cef385609c
FlynnTheInfinite <fabienmorival@gmail.com>
2019-11-15 11:04:44 +0100

    Added can't attack restriction

--- model/Card.js
+++ model/Card.js
@@ -460,7 +460,7 @@ class Card {
 
 		var eff = this.eff;
 
-		if (!this.isType("character") || !this.onBoard || !target.onBoard || this.area === target.area || eff.frozen || eff.atk <= 0 || eff.range <= 0 || target.concealed || this.hasState("static"))
+		if (!this.isType("character") || !this.onBoard || !target.onBoard || this.area === target.area || eff.frozen || eff.atk <= 0 || eff.range <= 0 || target.concealed || this.hasState("static") || this.hasState("passive"))
 			return false;
 		if (eff.firstTurn && !this.hasState("rush"))
 			return false;
