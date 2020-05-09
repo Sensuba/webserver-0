0c833283ee5588eaf1c2bcd4dcb9c7423df00077
Flynn <fabienmorival@gmail.com>
2020-05-09 09:17:20 +0200

    Modification de MoveNear

--- model/Blueprint/CheckLocation.js
+++ model/Blueprint/CheckLocation.js
@@ -6,7 +6,7 @@ class CheckLocation extends Bloc {
 	constructor (src, ctx) {
 
 		super("checkloc", src, ctx);
-		this.f = (src, ins) => [ins[1].includes(ins[0]), ins[0] !== null && ins[0] !== undefined];
+		this.f = (src, ins) => [ins[0] && ins[1].includes(ins[0]), ins[0] !== null && ins[0] !== undefined];
 		this.types = [Types.location, Types.locations];
 	}
 }
--- model/Blueprint/MoveNear.js
+++ model/Blueprint/MoveNear.js
@@ -9,27 +9,30 @@ class MoveNear extends Bloc {
 		this.f = (src, ins) => {
 			var tile = ins[1];
 			if (!tile.neighbors)
-				return [];
+				return [null];
 			if (tile.isEmpty) {
 				ins[0].goto(tile);
-				return [];
+				return [tile];
 			}
 			var neighbors = tile.neighbors.filter(t => t.isEmpty);
 			if (neighbors.length > 0) {
-				ins[0].goto(neighbors[Math.floor(Math.random()*neighbors.length)]);
-				return [];
+				var res = neighbors[Math.floor(Math.random()*neighbors.length)];
+				ins[0].goto(res);
+				return [res];
 			}
 			var adjacents = tile.adjacents.filter(t => t.isEmpty);
 			if (adjacents.length > 0) {
-				ins[0].goto(adjacents[Math.floor(Math.random()*adjacents.length)]);
-				return [];
+				var res = adjacents[Math.floor(Math.random()*adjacents.length)];
+				ins[0].goto(res);
+				return [res];
 			}
 			var all = tile.field.tiles.filter(t => t.isEmpty);
 			if (all.length > 0) {
-				ins[0].goto(all[Math.floor(Math.random()*all.length)]);
-				return [];
+				var res = all[Math.floor(Math.random()*all.length)];
+				ins[0].goto(res);
+				return [res];
 			}
-			return [];
+			return [null];
 		}
 		this.types = [Types.card, Types.location];
 	}
