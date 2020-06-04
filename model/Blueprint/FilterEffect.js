d2dbc6729bbb0f0ec95cc78b478e4f5609d87344
Flynn <fabienmorival@gmail.com>
2020-06-04 07:34:20 +0200

    Correction

--- model/Blueprint/FilterEffect.js
+++ model/Blueprint/FilterEffect.js
@@ -6,7 +6,7 @@ class FilterEffect extends Bloc {
 	constructor (src, ctx) {
 
 		super("filtereffect", src, ctx);
-		this.f = (src, ins) => [target => target && target.innereffects.some(e => ins[1](e)), model => model.blueprint.basis.some(basis => ins[1](model.blueprint[basis.type][basis.index]))];
+		this.f = (src, ins) => [target => target && target.innereffects.some(e => ins[0](e)), model => model.blueprint.basis.some(basis => ins[0](model.blueprint[basis.type][basis.index]))];
 		this.types = [Types.effecttype];
 	}
 }
