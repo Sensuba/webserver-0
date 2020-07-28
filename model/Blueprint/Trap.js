da100ad37ece19b98a8f725b468893b057dbe2f5
Flynn <fabienmorival@gmail.com>
2020-07-29 00:34:35 +0200

    Correction des listeners de lancement de sorts

--- model/Blueprint/Trap.js
+++ model/Blueprint/Trap.js
@@ -16,11 +16,25 @@ class Trap extends Bloc {
 		var that = this;
 		var listener = new Listener(owner, () => that.src.gameboard.subscribe("draw", (t,s,d) => {
 			if (d[0] === owner) {
-				owner.goto(owner.area.court);
-				owner.gameboard.notify("trap", owner);
-				that.execute({src: owner, image: image});
-				owner.destroy();
-				owner.gameboard.update();
+				var act = () => {
+					owner.goto(owner.area.court);
+					owner.gameboard.notify("trap", owner);
+					that.execute({src: owner, image: image});
+					owner.destroy();
+					owner.gameboard.update();
+				}
+				if (owner.area.court.isEmpty)
+					act();
+				else {
+					owner.goto(owner.area.capsule);
+					that.unsub = that.src.gameboard.subscribe("cardmove", (t2,s2,d2) => {
+						if (owner.area.court.isEmpty) {
+							act();
+							that.unsub();
+							delete that.unsub;
+						}
+					});
+				}
 			}
 		}), true);
 		owner.passives.push(listener);
