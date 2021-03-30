a9369e899fccbad84fa7c55eaae50e79bda5b859
Flynn <fabienmorival@gmail.com>
2021-03-30 23:57:28 +0200

    Correction

--- model/Blueprint/IsCovered.js
+++ model/Blueprint/IsCovered.js
@@ -6,7 +6,7 @@ class IsCovered extends Bloc {
 	constructor (src, ctx) {
 
 		super("covered", src, ctx);
-		this.f = (src, ins) => [ins[0].isCovered(false) || ins[0].isCovered(true), ins[0].isCovered(false), ins[0].isCovered(true)];
+		this.f = (src, ins) => [ins[0] && (ins[0].isCovered(false) || ins[0].isCovered(true)), ins[0] && ins[0].isCovered(false), ins[0] && ins[0].isCovered(true)];
 		this.types = [Types.card];
 	}
 }
