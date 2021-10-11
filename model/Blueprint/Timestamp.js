5b6bee4e4c1c99f4887fdd21de5ba56ddbc98a5c
Flynn <fabienmorival@gmail.com>
2021-10-11 12:02:25 +0200

    Correction

--- model/Blueprint/Timestamp.js
+++ model/Blueprint/Timestamp.js
@@ -7,11 +7,7 @@ class Timestamp extends Bloc {
 	constructor (src, ctx) {
 
 		super("timestamp", src, ctx);
-<<<<<<< HEAD
-		this.f = (src, ins) => [ new Event(src, ins[0].time ? "endturn" : "newturn", (t,s,d) => ins[0].player === null || (ins[0].player === 1 && s.isPlaying) || (ins[0].player === 2 && s.opposite.isPlaying)) ];
-=======
 		this.f = (src, ins) => [ new Event(src, ins[0].time ? "endturn" : "newturn", (t,s,d) => ins[0].player === null || ins[0].player.isPlaying) ];
->>>>>>> parent of dd6a521... Correction
 		this.types = [Types.timestamp];
 	}
 }
