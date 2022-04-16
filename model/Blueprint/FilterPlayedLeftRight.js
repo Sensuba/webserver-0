5de603d532e627f1da15359f672187bd4f2a5a38
Flynn <fabienmorival@gmail.com>
2022-04-16 15:29:11 +0200

    Suppression debug

--- model/Blueprint/FilterPlayedLeftRight.js
+++ model/Blueprint/FilterPlayedLeftRight.js
@@ -6,13 +6,7 @@ class FilterPlayedLeftRight extends Bloc {
 	constructor (src, ctx) {
 
 		super("filterplaylr", src, ctx);
-		this.f = (src, ins) => [target => {
-			if (target && target.playedFrom) {
-				console.log(ins);
-				console.log(target.playedFrom);
-			}
-			return target && target.playedFrom && (!ins[0] || target.playedFrom.left) && (!ins[1] || target.playedFrom.right)
-		}];
+		this.f = (src, ins) => [target => target && target.playedFrom && (!ins[0] || target.playedFrom.left) && (!ins[1] || target.playedFrom.right)];
 		this.types = [Types.bool, Types.bool];
 	}
 }
